9fe1e3c52324f0a4c8da348f89170d3ddd940fc2
Liikt <liikt@twatsquad.net>
2018-05-01 16:47:19 +0200

    updated the script a bit

--- updatebot.sh
+++ updatebot.sh
@@ -6,7 +6,7 @@ PULL=`git pull`
 
 if [[ ${PULL} != 'Already up-to-date.' ]]; then
     ./kill.sh || true
-    python3 ooyodo.py &
     echo -e '\nRestarted Ooyodo\n----------------------------------\n' >> log/ooyolog
     echo 'restarted'
+    python3 ooyodo.py
 fi
